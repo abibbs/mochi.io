9eb0cf4315fc93767152a7346936c48a78fca3bb
MKS-Student <mks.student@hackreactor.com>
2015-12-14 15:25:32 -0800

    Working API for stock lookup (company info)

--- app.js
+++ app.js
@@ -1,4 +1,42 @@
+'use strict';
+// http://dev.markitondemand.com/MODApis/
 angular.module('mochiApp',[])
-.controller('mochiCntrl',function($scope) {
-  $scope.mochi = '';
+
+// .config(['$httpProvider', function($httpProvider) {
+//   $httpProvider.defaults.useXDomain = true;
+//   $httpProvider.defaults.withCredentials = true;
+//   delete $httpProvider.defaults.headers.common["X-Requested-With"];
+//   $httpProvider.defaults.headers.common["Accept"] = "application/json";
+//   $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
+//   }
+// ])
+
+.controller('stockLookup',function($scope,$http) {
+  // $scope.lookup;
+  // $scope.search = undefined;
+
+  // $http.jsonp('http://dev.markitondemand.com/Api/v2/Lookup/jsonp?jsoncallback=JSON_CALLBACK&input="facebook"')
+  // .success(function(res) {
+  //   $scope.lookup = res[0];
+  //   console.dir($scope.lookup);
+  //   // console.log($scope.mochi);
+  // })
+
+  $scope.getLookup = function() {
+    if ($scope.search === undefined){
+    $scope.search = "facebook";
+    }
+
+    $http.jsonp('http://dev.markitondemand.com/Api/v2/Lookup/jsonp?jsoncallback=JSON_CALLBACK&input="'+$scope.search+'"')
+      .success(function(res) {
+      $scope.lookup = res[0];
+      console.dir($scope.lookup);
+      // console.log($scope.mochi);
+    })
+    // console.log($scope.search)
+  }
+})
+
+.controller('stockQuote',function($scope,$http) {
+
 });
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var moment = require('moment');
 var app = express();
-
+var cors = require('cors');
 
 app.use(express.static(__dirname + '/'));
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+// app.use(cors());
 app.get('/', function(req,res) {
   // var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   // console.log(moment().format('MMMM Do YYYY, h:mm:ss a') + ': ' + originalUrl + ' served to ' + ip);
