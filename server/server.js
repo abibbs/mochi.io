171489801456cd45f491cb1d54798e4d4109d030
Anthony Bibbs <anthonybibbs@gmail.com>
2016-01-18 12:25:32 -0800

    [feature] Added login route and html shell

--- server/server.js
+++ server/server.js
@@ -1,19 +1,27 @@
 var express = require('express');
 var moment = require('moment');
 var cors = require('cors');
+var morgan = require('morgan');
+var path = require('path');
 var app = express();
 
 app.use(express.static(__dirname + './../client'));
+app.use(morgan('combined'));
 app.use(cors());
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //   next();
 // });
+
+// Routes
 app.get('/', function(req,res) {
   // var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   // console.log(moment().format('MMMM Do YYYY, h:mm:ss a') + ': ' + originalUrl + ' served to ' + ip);
   res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.get('/login', function(req,res) {
+	res.sendFile(path.join(__dirname + './../client/login.html'));
 });
 
 var server = app.listen((process.env.PORT || 3000), function() {
