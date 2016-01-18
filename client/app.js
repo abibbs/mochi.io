db41d4452613b2499511cbd8233938075fdf200b
Anthony Bibbs <anthonybibbs@gmail.com>
2016-01-18 11:01:19 -0800

    [refactor] Refactor entire app for modularity

---
+++ client/app.js
@@ -0,0 +1,11 @@
+// http://dev.markitondemand.com/MODApis/
+angular.module('mochi',['mochi.ticker', 'mochi.tracker']);
+
+// .config(['$httpProvider', function($httpProvider) {
+//   $httpProvider.defaults.useXDomain = true;
+//   $httpProvider.defaults.withCredentials = true;
+//   delete $httpProvider.defaults.headers.common["X-Requested-With"];
+//   $httpProvider.defaults.headers.common["Accept"] = "application/json";
+//   $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
+//   }
+// ])
--- server/server.js
+++ server/server.js
@@ -3,17 +3,17 @@ var moment = require('moment');
 var cors = require('cors');
 var app = express();
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + './../client'));
+app.use(cors());
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //   next();
 // });
-// app.use(cors());
 app.get('/', function(req,res) {
   // var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   // console.log(moment().format('MMMM Do YYYY, h:mm:ss a') + ': ' + originalUrl + ' served to ' + ip);
-  res.render('index');
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 var server = app.listen((process.env.PORT || 3000), function() {
